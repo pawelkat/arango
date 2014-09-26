57dc6e02f8bc5a96f3b05d484be7703c3b58dbe7
unknown <dhwpaka@DASP303.dzwn.intern>
2014-09-26 16:29:39 +0200

    changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 (function() {
     "use strict";
-
+	var db = require("org/arangodb").db;
     var Foxx = require("org/arangodb/foxx"),
         controller = new Foxx.Controller(applicationContext)
 
-    controller.get("/hello/:name", function(req, res) {
-        res.set("Content-Type", "text/plain");
-        res.body = "Hello " + req.params("name");
-    }); 
+    controller.get("/api/v1/vegetables", function(req, res) {        
+		res.json(db.vegetables.toArray()); 
+    });
+	controller.post('/api/v1/vegetables', function (req, res) {
+		var content = JSON.parse(req.requestBody);
+		db.vegetables.save(content);
+		res.json(content);
+	});
+	
 
 }());
